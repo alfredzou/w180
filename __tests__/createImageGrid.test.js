af5d951fbca68f1f64c2d9826a51b368bd38dfe0
alfredzou <alfredzou@outlook.com>
2025-03-27 19:18:55 +0000

    added node configuration files and inital test

---
+++ __tests__/createImageGrid.test.js
@@ -0,0 +1,9 @@
+const createImageGrid = require('./html/createImageGrid');
+
+test("converts numbers into image paths"), () -> {
+    expect(createImageGrid([1,2,3])).toEqual([
+        "/images/MINESWEEPER_1.png",
+        "/images/MINESWEEPER_2.png",
+        "/images/MINESWEEPER_3.png",
+    ])
+}
