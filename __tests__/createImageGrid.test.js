aa76b3b5673b217b83414aacf6e3ede0c80987af
alfredzou <alfredzou@outlook.com>
2025-03-29 02:06:23 +0000

    began testing function using jest

--- __tests__/createImageGrid.test.js
+++ __tests__/createImageGrid.test.js
@@ -1,9 +1,9 @@
-const createImageGrid = require('./html/createImageGrid');
+import {createImageGrid} from '../js/w8.js';
 
-test("converts numbers into image paths"), () -> {
+test("converts numbers into image paths", () => {
     expect(createImageGrid([1,2,3])).toEqual([
         "/images/MINESWEEPER_1.png",
         "/images/MINESWEEPER_2.png",
         "/images/MINESWEEPER_3.png",
     ])
-}
+})
