bc5c41bca039af82fd6f41f13cd4c7cae2cca95c
alfredzou <alfredzou@outlook.com>
2025-04-03 21:15:08 +0000

    uncommented JS code to allow website to work. add type=module for html to enable website to run with export

--- js/w9.js
+++ js/w9.js
@@ -1,5 +1,5 @@
-// const canvas = document.getElementById("minesweeperCanvas");
-// const ctx = canvas.getContext("2d");
+const canvas = document.getElementById("minesweeperCanvas");
+const ctx = canvas.getContext("2d");
 
 export function createImageGrid(grid) {
     for (let i = 0; i < grid.length; i++) {
@@ -9,29 +9,29 @@ export function createImageGrid(grid) {
     return grid
 }
 
-// const imageGrid = createImageGrid(
-//     [
-//         1,2,3,4,5,6,
-//         1,2,3,4,5,6,
-//         1,2,3,4,5,6,
-//         1,2,3,4,5,6,
-//         1,2,3,4,5,6,
-//         1,2,3,4,5,6,
-//     ]
-// ) 
+const imageGrid = createImageGrid(
+    [
+        1,2,3,4,5,6,
+        1,2,3,4,5,6,
+        1,2,3,4,5,6,
+        1,2,3,4,5,6,
+        1,2,3,4,5,6,
+        1,2,3,4,5,6,
+    ]
+) 
 
-// for (let i=0; i<imageGrid.length; i++) {
-//     const img = new Image()
-//     img.src = imageGrid[i]                   
+for (let i=0; i<imageGrid.length; i++) {
+    const img = new Image()
+    img.src = imageGrid[i]                   
 
-//     img.onload = function () {            
-//         const scaleFactor = 0.3  
-//         const newWidth = img.width * scaleFactor
-//         const newHeight = img.height * scaleFactor
+    img.onload = function () {            
+        const scaleFactor = 0.3  
+        const newWidth = img.width * scaleFactor
+        const newHeight = img.height * scaleFactor
 
-//         const x = newWidth * (i%6)
-//         const y = newHeight * Math.floor(i/6)
+        const x = newWidth * (i%6)
+        const y = newHeight * Math.floor(i/6)
 
-//         ctx.drawImage(img, x, y, newWidth, newHeight)
-//     }
-// }     
+        ctx.drawImage(img, x, y, newWidth, newHeight)
+    }
+}     
