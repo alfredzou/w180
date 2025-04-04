0a004e3d677634589b62980291c4eb589ae557c4
alfredzou <alfredzou@outlook.com>
2025-04-04 19:23:22 +0000

    refined splitting up logic so it works now

--- js/w9_index.js
+++ js/w9_index.js
@@ -1,13 +1,7 @@
+import { createImageGrid, displayImageGrid } from "./w9_mod.js";
+
 const canvas = document.getElementById("minesweeperCanvas");
 const ctx = canvas.getContext("2d");
-
-export function createImageGrid(grid) {
-    for (let i = 0; i < grid.length; i++) {
-        grid[i] = `/images/MINESWEEPER_${grid[i]}.png`
-    }
-
-    return grid
-}
 
 const imageGrid = createImageGrid(
     [
@@ -20,18 +14,4 @@ const imageGrid = createImageGrid(
     ]
 ) 
 
-for (let i=0; i<imageGrid.length; i++) {
-    const img = new Image()
-    img.src = imageGrid[i]                   
-
-    img.onload = function () {            
-        const scaleFactor = 0.3  
-        const newWidth = img.width * scaleFactor
-        const newHeight = img.height * scaleFactor
-
-        const x = newWidth * (i%6)
-        const y = newHeight * Math.floor(i/6)
-
-        ctx.drawImage(img, x, y, newWidth, newHeight)
-    }
-}     
+displayImageGrid(imageGrid, ctx)
--- js/w9_mod.js
+++ js/w9_mod.js
@@ -1,6 +1,3 @@
-const canvas = document.getElementById("minesweeperCanvas");
-const ctx = canvas.getContext("2d");
-
 export function createImageGrid(grid) {
     for (let i = 0; i < grid.length; i++) {
         grid[i] = `/images/MINESWEEPER_${grid[i]}.png`
@@ -9,29 +6,20 @@ export function createImageGrid(grid) {
     return grid
 }
 
-const imageGrid = createImageGrid(
-    [
-        1,2,3,4,5,6,
-        1,2,3,4,5,6,
-        1,2,3,4,5,6,
-        1,2,3,4,5,6,
-        1,2,3,4,5,6,
-        1,2,3,4,5,6,
-    ]
-) 
+export function displayImageGrid(imageGrid, ctx) {
+    for (let i=0; i<imageGrid.length; i++) {
+        const img = new Image()
+        img.src = imageGrid[i]                   
 
-for (let i=0; i<imageGrid.length; i++) {
-    const img = new Image()
-    img.src = imageGrid[i]                   
+        img.onload = function () {            
+            const scaleFactor = 0.3  
+            const newWidth = img.width * scaleFactor
+            const newHeight = img.height * scaleFactor
 
-    img.onload = function () {            
-        const scaleFactor = 0.3  
-        const newWidth = img.width * scaleFactor
-        const newHeight = img.height * scaleFactor
+            const x = newWidth * (i%6)
+            const y = newHeight * Math.floor(i/6)
 
-        const x = newWidth * (i%6)
-        const y = newHeight * Math.floor(i/6)
-
-        ctx.drawImage(img, x, y, newWidth, newHeight)
-    }
-}     
+            ctx.drawImage(img, x, y, newWidth, newHeight)
+        }
+    }     
+}
