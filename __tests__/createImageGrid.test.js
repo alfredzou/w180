aa2cb4c2ef5eb2e842d11872ebd34e8a874e54bf
Alfred Zou <alfredzou@outlook.com>
2025-04-05 07:53:48 +1100

    enabled local testing, rather than on vm

--- __tests__/createImageGrid.test.js
+++ __tests__/createImageGrid.test.js
@@ -1,34 +1,34 @@
 import {createImageGrid} from '../js/w9_mod.js';
 
-test("converts numbers into image paths", () => {
-    expect(createImageGrid(
-        [
-            1,2,3,4,5,6,
-            1,2,3,4,5,6,
-            1,2,3,4,5,6,
-            1,2,3,4,5,6,
-            1,2,3,4,5,6,
-            1,2,3,4,5,6,
-        ]
-    )).toEqual(
-        [
-            "/images/MINESWEEPER_1.png","/images/MINESWEEPER_2.png","/images/MINESWEEPER_3.png","/images/MINESWEEPER_4.png","/images/MINESWEEPER_5.png","/images/MINESWEEPER_6.png",
-            "/images/MINESWEEPER_1.png","/images/MINESWEEPER_2.png","/images/MINESWEEPER_3.png","/images/MINESWEEPER_4.png","/images/MINESWEEPER_5.png","/images/MINESWEEPER_6.png",
-            "/images/MINESWEEPER_1.png","/images/MINESWEEPER_2.png","/images/MINESWEEPER_3.png","/images/MINESWEEPER_4.png","/images/MINESWEEPER_5.png","/images/MINESWEEPER_6.png",
-            "/images/MINESWEEPER_1.png","/images/MINESWEEPER_2.png","/images/MINESWEEPER_3.png","/images/MINESWEEPER_4.png","/images/MINESWEEPER_5.png","/images/MINESWEEPER_6.png",
-            "/images/MINESWEEPER_1.png","/images/MINESWEEPER_2.png","/images/MINESWEEPER_3.png","/images/MINESWEEPER_4.png","/images/MINESWEEPER_5.png","/images/MINESWEEPER_6.png",
-            "/images/MINESWEEPER_1.png","/images/MINESWEEPER_2.png","/images/MINESWEEPER_3.png","/images/MINESWEEPER_4.png","/images/MINESWEEPER_5.png","/images/MINESWEEPER_6.png",
-        ]
-    )
-
-
 // test("converts numbers into image paths", () => {
-//     expect(createImageGrid([1,2,3])).toEqual([
-//         "/images/MINESWEEPER_1.png",
-//         "/images/MINESWEEPER_2.png",
-//         "/images/MINESWEEPER_3.png",
-//     ])
-// })
+//     expect(createImageGrid(
+//         [
+//             1,2,3,4,5,6,
+//             1,2,3,4,5,6,
+//             1,2,3,4,5,6,
+//             1,2,3,4,5,6,
+//             1,2,3,4,5,6,
+//             1,2,3,4,5,6,
+//         ]
+//     )).toEqual(
+//         [
+//             "/images/MINESWEEPER_1.png","/images/MINESWEEPER_2.png","/images/MINESWEEPER_3.png","/images/MINESWEEPER_4.png","/images/MINESWEEPER_5.png","/images/MINESWEEPER_6.png",
+//             "/images/MINESWEEPER_1.png","/images/MINESWEEPER_2.png","/images/MINESWEEPER_3.png","/images/MINESWEEPER_4.png","/images/MINESWEEPER_5.png","/images/MINESWEEPER_6.png",
+//             "/images/MINESWEEPER_1.png","/images/MINESWEEPER_2.png","/images/MINESWEEPER_3.png","/images/MINESWEEPER_4.png","/images/MINESWEEPER_5.png","/images/MINESWEEPER_6.png",
+//             "/images/MINESWEEPER_1.png","/images/MINESWEEPER_2.png","/images/MINESWEEPER_3.png","/images/MINESWEEPER_4.png","/images/MINESWEEPER_5.png","/images/MINESWEEPER_6.png",
+//             "/images/MINESWEEPER_1.png","/images/MINESWEEPER_2.png","/images/MINESWEEPER_3.png","/images/MINESWEEPER_4.png","/images/MINESWEEPER_5.png","/images/MINESWEEPER_6.png",
+//             "/images/MINESWEEPER_1.png","/images/MINESWEEPER_2.png","/images/MINESWEEPER_3.png","/images/MINESWEEPER_4.png","/images/MINESWEEPER_5.png","/images/MINESWEEPER_6.png",
+//         ]
+//     )
+
+
+test("converts numbers into image paths", () => {
+    expect(createImageGrid([1,2,3])).toEqual([
+        "/images/MINESWEEPER_1.png",
+        "/images/MINESWEEPER_2.png",
+        "/images/MINESWEEPER_3.png",
+    ])
+})
 
 // test("acceptable values only", () => {
 //     expect(createImageGrid([1,2,3,4,5,6,7,8])).toEqual([
