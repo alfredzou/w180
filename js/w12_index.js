6583c04cb1bce4f29dfd201c6f18b36a684a49e6
Alfred Zou <alfredzou@outlook.com>
2025-04-26 05:45:05 +1000

    started w12 based on w11

---
+++ js/w12_index.js
@@ -0,0 +1,17 @@
+import { createImageGrid, displayImageGrid } from "./w11_mod.js";
+
+const canvas = document.getElementById("minesweeperCanvas");
+const ctx = canvas.getContext("2d");
+
+const imageGrid = createImageGrid(
+    [
+        1,2,3,4,5,6,
+        1,2,3,4,5,6,
+        1,2,3,4,5,6,
+        1,2,3,4,5,6,
+        1,2,3,4,5,6,
+        1,2,3,4,5,6,
+    ]
+) 
+
+displayImageGrid(imageGrid, ctx)
